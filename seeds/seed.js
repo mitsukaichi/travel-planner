15a0c77bda6e765540b8d940e9a966629d823a0a
Minami Itsukaichi <mitukaichi@gmail.com>
2023-11-02 11:46:21 -0700

    change folder name and file locations in seeds folder

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,8 @@
 const sequelize = require('../config/connection');
 const { Traveller, Location, Trip } = require('../models');
 
-const travellerSeedData = require('../travellerSeedData.json');
-const locationSeedData = require('../locationSeedData.json');
+const travellerSeedData = require('./travellerSeedData.json');
+const locationSeedData = require('./locationSeedData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
