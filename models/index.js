f1b300477a92b6a933e890ec9f5c83a7642ab59a
Minami Itsukaichi <mitukaichi@gmail.com>
2023-11-02 11:52:13 -0700

    update index.js

--- models/index.js
+++ models/index.js
@@ -4,12 +4,14 @@ const Location = require('./Location');
 const Trip = require('./Trip');
 
 //travellers have many locations
-Traveller.hasMany(Location);
+//Traveller.hasMany(Location);
 
 //locations have many travellers through trips
 
 // Location.hasMany(Traveller, {through: Trip });
 Location.belongsToMany(Traveller, {through: Trip });
+
+Traveller.belongsToMany(Location, {through: Trip });
 
 module.exports = {
     Traveller,
